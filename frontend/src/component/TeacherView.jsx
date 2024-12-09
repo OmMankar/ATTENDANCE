d7a54fc6c8722ee36da7dc8a357fc5d517b26a53
OmMankar <138040408+OmMankar@users.noreply.github.com>
2024-12-09 19:00:20 +0530

    Update TeacherView.jsx

--- frontend/src/component/TeacherView.jsx
+++ frontend/src/component/TeacherView.jsx
@@ -13,7 +13,7 @@ const TeacherView=()=>{
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-    fetch("http://localhost:5000/api/v1/teacher/fetchStudent", {
+    fetch("https://attendance-backend-wask.onrender.com/api/v1/teacher/fetchStudent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
